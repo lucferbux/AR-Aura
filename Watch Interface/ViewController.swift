fa54ec4f9c11ea724731d83c1c6a8808b1fe597d
lfernandezaragon <lucasfernandezaragon@gmail.com>
2018-10-05 11:20:15 +0200

    First version.

--- Watch Interface/ViewController.swift
+++ Watch Interface/ViewController.swift
@@ -10,66 +10,76 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
+        
+        // Create a new Reference tracking dataset
+        guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: Bundle.main) else { return }
+        
+        // Set the image tracking and number of tracking images
+        configuration.trackingImages = referenceImages
+        configuration.maximumNumberOfTrackedImages = 1
+        configuration.isLightEstimationEnabled = true
 
-        // Run the view's session
+        // Run the view's session with lightning estimation
         sceneView.session.run(configuration)
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
         // Pause the view's session
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
+}
+
+extension ViewController: ARSCNViewDelegate {
+
+    //      Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         let node = SCNNode()
-     
+        
+        // Check if the anchor is and image anchor
+        guard anchor is ARImageAnchor else { return nil }
+        // Create the plane
+        let plane = SCNPlane(width: 0.1, height: 0.05)
+        let interfaceScene = SKScene(fileNamed: "InterfaceScene")
+        // Convert the material of the plane into the SpriteKit scene
+        plane.firstMaterial?.diffuse.contents = interfaceScene
+        plane.firstMaterial?.isDoubleSided = true
+        // Transform the plane to display it horizontally
+        plane.firstMaterial?.diffuse.contentsTransform = SCNMatrix4Translate(SCNMatrix4MakeScale(1, -1, 1), 0, 1, 0)
+        let planeNode = SCNNode(geometry: plane)
+        planeNode.eulerAngles.x = -.pi / 2
+        
+        // Create the 3D scene kit's model
+        var auraNode = SCNNode()
+        let auraScene = SCNScene(named: "art.scnassets/AuraNew.scn")!
+        // Add it in position
+        auraNode = auraScene.rootNode.childNodes.first!
+        auraNode.position = SCNVector3(0, -0.012, 0.01)
+        // Add an scale
+        let scale = 0.008
+        auraNode.scale = SCNVector3(scale, scale, scale)
+        
+        node.addChildNode(planeNode)
+        planeNode.addChildNode(auraNode)
+
         return node
     }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+
 }
